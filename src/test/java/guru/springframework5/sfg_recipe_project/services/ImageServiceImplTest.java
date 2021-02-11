269b9aeb273c08c9fe554f25d880c5c5d6906a0f
Your Name <prernark@gmail.com>
2021-02-11 12:37:17 +0000

    Saved image to database

--- src/test/java/guru/springframework5/sfg_recipe_project/services/ImageServiceImplTest.java
+++ src/test/java/guru/springframework5/sfg_recipe_project/services/ImageServiceImplTest.java
@@ -1,20 +1,51 @@
 package guru.springframework5.sfg_recipe_project.services;
 
+import guru.springframework5.sfg_recipe_project.domain.Recipe;
+import guru.springframework5.sfg_recipe_project.repositories.RecipeRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(SpringExtension.class)
 class ImageServiceImplTest {
-    ImageService imageService;
-
+    private ImageService imageService;
+    @Mock
+    private RecipeRepository recipeRepository;
 
     @BeforeEach
     void setUp() {
+        imageService = new ImageServiceImpl(recipeRepository);
     }
 
     @Test
-    void saveImageFile() {
+    void saveImageFile() throws IOException {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        MockMultipartFile mockMultipartFile = new MockMultipartFile("imgFile", "texting.txt",
+                                                                "text/plain", "SpringFrameworkGuru".getBytes());
+
+        ArgumentCaptor<Recipe> argumentCaptor = ArgumentCaptor.forClass(Recipe.class);
+        //when
+        imageService.saveImageFile(1L, mockMultipartFile);
+
+        //then
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, times(1)).save(argumentCaptor.capture());
+        Recipe savedRecipe = argumentCaptor.getValue();
+        assertEquals(mockMultipartFile.getBytes().length, savedRecipe.getImage().length);
     }
 }
