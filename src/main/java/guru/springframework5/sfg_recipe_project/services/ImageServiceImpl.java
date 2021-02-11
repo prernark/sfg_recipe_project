52fde60089410aa116c92eb3aff85c9a7bc41ef5
Your Name <prernark@gmail.com>
2021-02-11 12:36:22 +0000

    Save the image into the database

--- src/main/java/guru/springframework5/sfg_recipe_project/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework5/sfg_recipe_project/services/ImageServiceImpl.java
@@ -1,15 +1,39 @@
 package guru.springframework5.sfg_recipe_project.services;
 
+import guru.springframework5.sfg_recipe_project.domain.Recipe;
+import guru.springframework5.sfg_recipe_project.repositories.RecipeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.transaction.Transactional;
+import java.io.IOException;
+
 @Slf4j
 @Service
-public class ImageServiceImpl implements ImageService{
+public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
+    public ImageServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
 
     @Override
+    @Transactional
     public void saveImageFile(Long recipeId, MultipartFile file) {
-        log.debug("Image file received "+file.getOriginalFilename());
+        log.debug("Image file received " + file.getOriginalFilename());
+        Recipe recipe = recipeRepository.findById(recipeId).get();
+        try {
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+            int i = 0;
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+            recipe.setImage(byteObjects);
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
